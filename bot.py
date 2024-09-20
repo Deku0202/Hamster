5d3d04cddac60814d8745d7abbd2e6afa60c09ab
Deku <linmyathu195@gmail.com>
2024-09-20 18:46:34 -0500

    file location move

--- bot.py
+++ bot.py
@@ -7,8 +7,14 @@ from datetime import datetime
 import random
 import json
 
-# Get the directory where the script is located
-main_dir = os.path.dirname(os.path.realpath(__file__))
 
-# Get the directory of the data file
-data_dir = os.path.join(main_dir, "data.json")
+
+class Hmaster:
+    def __init__(self):
+        # Get the directory where the script is located
+        main_dir = os.path.dirname(os.path.realpath(__file__))
+
+        # Get the directory of the data file
+        data_dir = os.path.join(main_dir, "data.json")
+
+    
