b19df88cd4423fda54606773d79ac91ab2e4d638
Deku <linmyathu195@gmail.com>
2024-09-20 18:53:22 -0500

    config

--- bot.py
+++ bot.py
@@ -12,9 +12,12 @@ import json
 class Hmaster:
     def __init__(self):
         # Get the directory where the script is located
-        main_dir = os.path.dirname(os.path.realpath(__file__))
+        self.main_dir = os.path.dirname(os.path.realpath(__file__))
 
         # Get the directory of the data file
-        data_dir = os.path.join(main_dir, "data.json")
+        self.data_dir = os.path.join(self.main_dir, "data.json")
+
+        # Get the directory of the config file
+        self.config = os.path.join(self.main_dir, "config.json")
 
     
